efe6ec659dffb47589fe33a6d7a23bbe6e898cdc
Tommi Kivinen <tommi.kivinen@tuni.fi>
2022-02-07 21:33:08 +0200

    Reformat files

--- cards.py
+++ cards.py
@@ -5,6 +5,7 @@ Created on Wed Jan 19 19:01:03 2022
 @author: Tommi Kivinen
 """
 
+
 #
 # Card 2-10
 #
@@ -13,17 +14,18 @@ class Card:
     def __init__(self, val):
         # self.suit = suit
         self.value = [val, val]
-        
+
     def show(self):
         print(self.value[0])
-        
+
+
 #
 # Ace, either 1 or 11
 #
 
 class Ace:
     def __init__(self):
-        self.value = [1,11]
-        
+        self.value = [1, 11]
+
     def show(self):
         print(f"{self.value[0]} / {self.value[1]}")
--- dealer.py
+++ dealer.py
@@ -5,6 +5,7 @@ Created on Wed Jan 19 19:05:03 2022
 @author: Tommi Kivinen
 """
 
+
 #
 # Jakaja
 #
@@ -13,28 +14,28 @@ class Dealer:
 
     def __init__(self):
         self.hand = []
-        
+
     def draw(self, deck):
         self.hand.append(deck.drawcard())
         return self
-    
+
     def resethand(self):
         self.hand = []
-    
+
     def showhand_beginning(self):
         print("(Piilotettu kortti)")
         self.hand[1].show()
-            
+
     def showhand(self):
         for card in self.hand:
             card.show()
         print("=")
         print(self.count_total())
-    
+
     def count_total(self):
         total = [0, 0]
         for card in self.hand:
             total[0] += card.value[0]
             total[1] += card.value[1]
-            
+
         return total
