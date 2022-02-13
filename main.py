9c6c6b1397c7b3047f43eb181787d96d5541b97f
Tommi Kivinen <tommi.kivinen@tuni.fi>
2022-02-13 16:12:56 +0200

    add "if name is main then main()"

--- main.py
+++ main.py
@@ -9,48 +9,54 @@ from player import Player
 from dealer import Dealer
 from game import blackjackgame
 
-# Welcome user
-print("Tervetuloa pelaamaan BlackJackia!")
 
-print("Syötä nimesi:")
-playerName = input()
-    
-print(f"Moro {playerName}! Tarvitaan alkusijoitus. Paljonko pelimerkkejä laitetaan?")
-startDeposit = int(input())
-    
-print("Kiitos! Voidaan aloittaa peli.")
-player = Player(playerName, startDeposit)
-dealer = Dealer()
+def main():
+    # Welcome user
+    print("Tervetuloa pelaamaan BlackJackia!")
 
-play = True
+    print("Syötä nimesi:")
+    playerName = input()
 
-while play:
-    
-    # Betting stage
-    print("Aseta panos:")
-    bet = int(input())
-    bankSituation = player.showbank()
-    if bet > bankSituation:
-        print("Panoksesi on enemmän kuin mitä sinulla on tilillä.")
-        play = False
-        break
+    print(f"Moro {playerName}! Tarvitaan alkusijoitus. Paljonko pelimerkkejä laitetaan?")
+    startDeposit = int(input())
 
-    blackjackgame(player, dealer, bet)
-    bankSituation = player.showbank()
-    print(f"Pelimerkkejä jäljellä {bankSituation}.")    
-    print("Pelataanko uudelleen (y)?")
-    answer = input()
-    
-    if answer == 'y':
-        if bankSituation > 0:
-            print("OK! Jaetaan kortit.")
-            # play = True
+    print("Kiitos! Voidaan aloittaa peli.")
+    player = Player(playerName, startDeposit)
+    dealer = Dealer()
+
+    play = True
+
+    while play:
+
+        # Betting stage
+        print("Aseta panos:")
+        bet = int(input())
+        bankSituation = player.showbank()
+        if bet > bankSituation:
+            print("Panoksesi on enemmän kuin mitä sinulla on tilillä.")
+            play = False
+            break
+
+        blackjackgame(player, dealer, bet)
+        bankSituation = player.showbank()
+        print(f"Pelimerkkejä jäljellä {bankSituation}.")
+        print("Pelataanko uudelleen (y)?")
+        answer = input()
+
+        if answer == 'y':
+            if bankSituation > 0:
+                print("OK! Jaetaan kortit.")
+                # play = True
+
+            else:
+                print("Pelitilisi on tyhjä, hävisit kaiken. Kasino kiittää!")
+                play = False
 
         else:
-            print("Pelitilisi on tyhjä, hävisit kaiken. Kasino kiittää!")
+            print("Kiitos pelaamisesta!")
+            print(f"Pelitilillesi jäi {bankSituation} pelimerkkiä.")
             play = False
-        
-    else:
-        print("Kiitos pelaamisesta!")
-        print(f"Pelitilillesi jäi {bankSituation} pelimerkkiä.")
-        play = False
+
+
+if __name__ == "__main__":
+    main()
